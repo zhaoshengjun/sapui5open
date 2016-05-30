64149d8dbf43f6cc1c30bbaf0ac0d3182bd8dc99
Sam Zhao <zhao.shengjun@gmail.com>
2016-05-30 06:08:18 +0000

    week 1 exercise 4

---
+++ MyApp/webapp/Component.js
@@ -0,0 +1,15 @@
+sap.ui.define(
+	[
+		"sap/ui/core/UIComponent"
+	], 
+	function(UIComponent){
+		return UIComponent.extend("opensap.myapp.Component", {
+			metadata: {
+				manifest:'json'
+			},
+			
+			init: function() {
+				UIComponent.prototype.init.apply(this, arguments);
+			}
+		});
+});
